f40b5e4d92ce9e5a01498e331f3812834404064a
ncwuping <ncwuping@hotmail.com>
2019-04-26 12:58:21 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
 
-echo '[{"name": "root", "account": "admin", "password": ""}, {"name": "maxscale", "account": "admin", "password": ""}]' > /var/lib/maxscale/maxadmin-users
-/usr/bin/maxscale -d
+if [ -z "${SERVER2_ADDR}" -o -z "${SERVER3_ADDR}" ]; then
+  echo "No sufficient nodes for read-write split service"
+  exit 1
+fi
+
+if [ -n "${SERVER1_ADDR}" ]; then
+  sed -e 's!<ip_addr_of_server1>!'"${SERVER1_ADDR}"'!' -i /var/lib/maxscale/maxscale.cnf
+fi
+
+sed -e 's!<ip_addr_of_server2>!'"${SERVER2_ADDR}"'!' \
+    -e 's!<ip_addr_of_server3>!'"${SERVER3_ADDR}"'!' \
+    -i /var/lib/maxscale/maxscale.cnf
+
+if [ ! -f /var/lib/maxscale/.secrets ]; then
+  chroot --userspec=maxscale:maxscale /usr/bin/maxkeys
+fi
+
+cipher=$( maxpasswd /var/lib/maxscale ${MAXSCALE_SECRET:-"maxscale-proxy"} )
+sed -e 's!<cipher_of_secret>!'"${cipher}"'!g' -i /var/lib/maxscale/maxscale.cnf
+
+/usr/bin/maxscale -d -U maxscale -f /var/lib/maxscale/maxscale.cnf
