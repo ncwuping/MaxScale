1c576b3cf6b07c831713887f52c33cdcd1c1bd3c
ncwuping <ncwuping@hotmail.com>
2020-08-31 23:05:55 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,11 +8,16 @@ fi
 chown maxscale:maxscale /var/lib/maxscale/{maxscale.cnf,maxadmin-users}
 
 if [ -n "${SERVER1_ADDR}" ]; then
-  sed -e 's!<ip_addr_of_server1>!'"${SERVER1_ADDR}"'!' -i /var/lib/maxscale/maxscale.cnf
+  sed -e 's!<ip_addr_of_server1>!'"${SERVER1_ADDR}"'!' \
+      -e 's!<port_of_server1>!'"${SERVER1_PORT:-3306}"'!' \
+      -e 's!server2,server3!server1,server2,server3!' \
+      -i /var/lib/maxscale/maxscale.cnf
 fi
 
 sed -e 's!<ip_addr_of_server2>!'"${SERVER2_ADDR}"'!' \
+    -e 's!<port_of_server2>!'"${SERVER2_PORT:-3306}"'!' \
     -e 's!<ip_addr_of_server3>!'"${SERVER3_ADDR}"'!' \
+    -e 's!<port_of_server3>!'"${SERVER3_PORT:-3306}"'!' \
     -i /var/lib/maxscale/maxscale.cnf
 
 if [ ! -f /var/lib/maxscale/.secrets ]; then
